b9dccfbe36a70f1e7a063b63456f4cd474cea6f9
kanakroy13 <120321818+kanakroy13@users.noreply.github.com>
2025-04-08 15:12:50 +0530

    Create FullAdder.v

---
+++ FullAdder.v
@@ -0,0 +1,8 @@
+// Full Adder module
+module FullAdder (
+    input  A, B, Cin,
+    output Sum, Cout
+);
+    assign Sum = A ^ B ^ Cin;
+    assign Cout = (A & B) | (B & Cin) | (Cin & A);
+endmodule
